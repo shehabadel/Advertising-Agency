fbf3ea0093091fb6eeda9e8fb982a5abd855ffe9
shehabadel <shehabeldinadel@gmail.com>
2021-09-07 11:36:17 +0200

    Finished, supposed to be

--- script.js
+++ script.js
@@ -1,3 +1,85 @@
+/*Owl Carousels */
+
+var owlWelcome = $('.owl-welcome');
+owlWelcome.owlCarousel({
+    animateOut: 'animate__fadeOut animate__zoomOut ',
+    animateIn: 'animate__fadeIn animate__zoomIn animate__slow',
+    items: 1,
+    loop: true,
+    nav: true,
+    autoplay: true,
+    autoplayTimeout: 6000,
+    autoplayHoverPause: true
+});
+var owlTeam = $('.owl-team');
+owlTeam.owlCarousel({
+    animateOut: 'animate__fadeOut animate__zoomOut ',
+    animateIn: 'animate__fadeIn animate__zoomIn animate__slow',
+    items: 2,
+    loop: true,
+    nav: true,
+    autoplay: true,
+    autoplayTimeout: 6000,
+    autoplayHoverPause: true,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+        },
+        600: {
+            items: 2,
+        },
+        1000: {
+            items: 3,
+            nav: true,
+            loop: true
+        }
+    }
+});
+var owlTestimonials=$('.owl-testimonials');
+owlTestimonials.owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay: true,
+    autoplayTimeout: 6000,
+    autoplayHoverPause: true,
+    responsiveClass: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        700:{
+            items:2
+        },
+        1000:{
+            items:2
+        }
+    }
+});
+
+var owlArticles= $('.owl-articles');
+owlArticles.owlCarousel({
+    loop:true,
+    margin:30,
+    nav:false,
+    autoplay: true,
+    autoplayTimeout: 6000,
+    autoplayHoverPause: true,
+    responsiveClass: true,
+    responsive:{
+        0:{
+            items:2
+        },
+        700:{
+            items:4
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+/*End Owl Carousels*/
 /*Start Navigation Bar*/
 $(document).ready(function () {
     $('.dropdown-toggle').addClass('without-after');
@@ -5,28 +87,27 @@ $(document).ready(function () {
 
 var navbarDropdownUl = $("#navbar-dropdown-ul");
 var navbarDropDown = $("#navbar-dropdown");
-
+//TODO Check the lag here
 $(document).on('mouseenter', '#navbar-dropdown', function () {
     navbarDropDown.addClass('show');
     navbarDropDown.attr('aria-expanded', 'true');
     navbarDropdownUl.addClass('show');
-    //TODO Ask Mina how can we make it animate over and over
     navbarDropdownUl.animate({
         'top': '50px',
         'opacity': 1
     }, 400, 'linear');
 });
 $(document).on('mouseleave', '#navbar-dropdown', function () {
-    navbarDropdownUl.animate({
-        'top': '120px',
-        'opacity': 0
-    }, 400, 'linear');
+
     setTimeout(function () {
         navbarDropDown.removeClass('show');
         navbarDropDown.attr('aria-expanded', 'false');
         navbarDropdownUl.removeClass('show');
     }, 200);
-
+    navbarDropdownUl.animate({
+        'top': '120px',
+        'opacity': 0
+    }, 400, 'linear');
 
 });
 $(document).on('click', '#navbar-dropdown', function () {
@@ -37,55 +118,51 @@ $(document).on('click', '#navbar-dropdown', function () {
 /*End Navigation Bar*/
 /*Start Welcome Section*/
 
-//TODO ASK KARAM HWA MESH SHA3'AL LEEEEH
-var owlNext = $(".owl-next");
-var owlPrev = $(".owl-prev");
-owlNext.on('mouseenter', function () {
-
-    owlNext.animate({
-        'background-color': '#536DFE'
-    }, 400, 'linear');
-});
-owlPrev.on('mouseenter', function () {
-    owlPrev.animate({
-        'background-color': '#536DFE'
-    }, 400, 'linear');
-});
-
-owlNext.on('mouseleave', function () {
-    owlNext.animate({
-        'background-color': '#7777773b'
-    }, 400, 'linear');
-});
-owlPrev.on('mouseleave', function () {
-    owlPrev.animate({
-        'background-color': '#7777773b'
-    }, 400, 'linear');
-});
 /*End Welcome Section*/
 
 /*Start Second Section*/
 
-var discoverMoreBtn = $("#discover-more-btn");
-var discoverMoreBtnText = $("#discover-more-btn-text");
+var discoverMoreBtn = $(".discover-more-btn");
+var discoverMoreBtnText = $(".discover-more-btn-text");
 
-$(document).on('mouseenter', "#discover-more-btn", function () {
+$(document).on('mouseenter', ".discover-more-btn", function () {
 
+    if($(this).parent().hasClass('invite-button'))
+    {
+        discoverMoreBtn.animate({
+            'background-color': 'white'
+        }, 600, 'linear');
+        discoverMoreBtnText.animate({
+            'color': '#222429'
+        }, 600, 'linear');
+    }else{
     discoverMoreBtn.animate({
         'background-color': 'black'
     }, 600, 'linear');
     discoverMoreBtnText.animate({
         'color': 'white'
-    }, 600, 'linear')
-})
-$(document).on('mouseleave', "#discover-more-btn", function () {
+    }, 600, 'linear');
+    }
+});
+$(document).on('mouseleave', ".discover-more-btn", function () {
+    if($(this).parent().hasClass('invite-button'))
+    {
+        discoverMoreBtn.animate({
+            'background-color': '#222429'
+        }, 600, 'linear');
+        discoverMoreBtnText.animate({
+            'color': 'white'
+        }, 600, 'linear')
+    }
+    else{
     discoverMoreBtn.animate({
         'background-color': '#536DFE'
     }, 600, 'linear');
     discoverMoreBtnText.animate({
         'color': 'black'
-    }, 600, 'linear')
-})
+    }, 600, 'linear');
+    }
+});
 /*End Second Section*/
 
 /*Start What We Offering Section*/
@@ -143,10 +220,10 @@ expUsOuter.on('mouseleave', function () {
     }, 200, 'linear')
 })
 /* End Experience Us Section */
-//TODO Ask Karam about owl-buttons animations
+
 /*Start Services Section*/
 $(function () {
-    $('[data-toggle="tooltip"]').tooltip({ trigger: 'manual' }).tooltip('show');
+    $('[data-toggle="tooltip"]').tooltip({ trigger: 'manual' }).tooltip('hide');
 });
 
 $(window).on('scroll', function () {
@@ -158,16 +235,106 @@ $(window).on('scroll', function () {
 
     //  }  
 });
-var portfolioItem= $('.portfolio-item');
+var portfolioItem = $('.portfolio-item');
 portfolioItem.on('mouseover', function () {
-    var toast = $(this).find('#portfolio-item-toast');
+    var toast = $(this).find('.portfolio-item-toast');
     toast.removeClass('hide');
     toast.addClass('animate__fadeInUp');
     toast.addClass('show');
 });
-portfolioItem.on('mouseleave',function () {
-    var toast = $(this).find('#portfolio-item-toast');
+portfolioItem.on('mouseleave', function () {
+    var toast = $(this).find('.portfolio-item-toast');
     toast.removeClass('animate__fadeInUp show');
     toast.addClass('hide');
 })
 /*End Services Section*/
+/*Start Clients Section*/
+var counterItemsH2 = $('counter-item').find('h2');
+var counterItemsH2Title = ['PROJECTS COMPLETED', 'ACTIVE CLIENTS', 'CUPS OF COFFEE', 'HAPPY CLIENTS'];
+var counterItemsList = $("#counter-items-list").children();
+$(document).ready(function () {
+
+    //removing border of last element
+    let lastCounterItem = counterItemsList.last();
+    lastCounterItem.removeClass('border-end');
+    if (window.matchMedia('(max-width: 1199px)').matches)
+    {
+        counterItemsList.eq(1).removeClass('border-end');
+    }
+    //Adding text-center and margin top to each counter item
+    $('.counter-item').each(function () {
+        $(this).addClass('text-center mt-5')
+    })
+
+});
+
+
+$('.count').each(function () {
+    $(this).prop('Counter', 0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 3000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
+
+$('.feature-card-text').on('mouseenter', function () {
+    $(this).parent().find('.feature-card-rounded').animate({
+        'background-color': '#212529'
+    }, 400, 'linear')
+    $(this).parent().find('.feature-card-rounded')
+        .find('.feature-card-rounded-text')
+        .animate({
+            'color': 'white'
+        }, 400, 'linear')
+});
+$('.feature-card-text').on('mouseleave', function () {
+    $(this).parent().find('.feature-card-rounded').animate({
+        'background-color': '#9e99ff'
+    }, 400, 'linear')
+    $(this).parent().find('.feature-card-rounded')
+        .find('.feature-card-rounded-text')
+        .animate({
+            'color': '#212529'
+        }, 400, 'linear')
+});
+/*End Clients Section*/
+/*Start Team Section*/
+/*End Team Section*/
+/*Start Customer Section*/
+var customerCard=$('.owl-testimonials').find('.item');
+customerCard.on('mouseenter',function () {
+    $(this).find('.customer-feedback').animate({
+        'background-color':'#536DFE'
+    },400,'linear');
+    $(this).find('span').animate({
+        'color':'#222429'
+    });
+    $(this).find('h6').animate({
+        'color':'#222429'
+    },400,'linear');
+    $(this).find('img').css({
+        'filter':'grayscale(0)'
+    },400,'linear');
+});
+customerCard.on('mouseleave',function () {
+    $(this).find('.customer-feedback').animate({
+        'background-color':'white'
+    },400,'linear');
+    $(this).find('span').animate({
+        'color':'#536DFE'
+    });
+    $(this).find('h6').animate({
+        'color':'#536DFE'
+    },400,'linear');
+    $(this).find('img').css({
+        'filter':'grayscale(1)'
+    },400,'linear');
+});
+/*End Customer Section*/
+/*Start Article Section*/
+
+/*End Article Section*/
